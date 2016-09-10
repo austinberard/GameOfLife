37f8f0c38b572eff79dac02ee3702c863ee976e2
Austin Berard <austinberard@outlook.com>
2016-09-10 17:10:38 -0400

    added a print board function and main loop to run the game

--- src/main.java
+++ src/main.java
@@ -2,4 +2,36 @@
  * Created by austinberard on 9/10/16.
  */
 public class main {
+    public static void main(String[] args) throws InterruptedException{
+        boolean[][] board = {{false, false, false, false, false},
+                             {false, false, true, false, false},
+                             {false, false, true, false, false},
+                             {false, false, true, false, false},
+                             {false, false, false, false, false},
+                             };
+        TorusBoard gameBoard = new TorusBoard(board);
+        int FRAMES = 10;
+        while(FRAMES > 0){
+            printBoard(gameBoard);
+            GameLogic.run(gameBoard);
+            FRAMES -= 1;
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void printBoard(TorusBoard board){
+        for (int i = 0; i < 50; ++i) System.out.println(); //clears screen...kinda TODO do this better
+        boolean[][] curr = board.getCurrentBoard();
+        for(int i = 0; i < curr.length; i++){
+            for(int j = 0; j< curr[i].length; j++){
+                if(board.isAlive(i, j)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
